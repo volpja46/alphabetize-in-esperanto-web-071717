dd2cfa4576add042d2b1fd49a238311041e2ae62
Jennifer Volpe <jennifervolpe@Jennifers-MBP.home>
2017-07-15 01:08:36 -0400

    gooo

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(expressions)
+    alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+    
+    expressions.sort_by do |phrase|
+        phrase.chars.map do |char|
+            alpha.index(char)
+            end
+    end
 end
